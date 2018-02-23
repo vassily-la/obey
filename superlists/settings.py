d76a1724c1fd3cb72080ddc6708d77a68f6b4f55
Vassily <vasil.deville@gmail.com>
2018-02-23 15:14:48 +0300

    Add localhost, 127 to the allowed

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ip(3a2ev0*nbv_6)lq(euj#cp$l+)0g(h6$u25pkzpl4tk*m2n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['188.166.11.15',]
+ALLOWED_HOSTS = ['188.166.11.15', '127.0.0.1', 'localhost',]
 
 # Application definition
 
